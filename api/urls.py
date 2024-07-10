d83b3586813f329e8b076d9cf65fb026a8a4bfb0
shreyansjaindev <shreyansjainrs@gmail.com>
2024-07-10 23:27:04 +0530

    Bug Fixes

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("", views.GetRoutes.as_view(), name="api-routes"),
     path("intelligence-harvester/", include("intelligence_harvester.urls")),
     path("domain-monitoring/", include("domain_monitoring.urls")),
-    path("frontend/", include("frontend.urls")),
 ]
